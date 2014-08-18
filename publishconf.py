bb7b71ea295e32004ec4bdda2693de483378a23f
David Schryer <schryer@gmail.com>
2014-08-18 19:00:38 +0300

    Removed site URL bug.

--- publishconf.py
+++ publishconf.py
@@ -10,7 +10,7 @@ import sys
 sys.path.append(os.curdir)
 from pelicanconf import *
 
-SITEURL = 'http://schryer.github.io'
+SITEURL = 'http://schryer.github.io/schryer_pelican_blog'
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
